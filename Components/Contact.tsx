2767d38354f0c370365da2e3798fc88b91f86401
charans29 <smodugula@csuchico.edu>
2024-07-03 20:42:55 -0700

    dev: design hero page

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Spotlight } from './UI/spotlight'
+import { Spotlight } from './UI/Spotlight'
 
 function Contact() {
   return (
-    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-screen w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
-      />Spotlight</div>
+      />Spotlight
+      <div className="absolute top-0 left-1/2 transform -translate-x-7/2 w-0.5 h-full bg-gray-600"></div>
+    </div>
   )
 }
 
--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Spotlight } from './UI/spotlight';
 
 function Hero() {
   return (
-    <div className='h-screen overflow-hidden'
-    style={{
+    <div
+      className="h-screen overflow-hidden"
+      style={{
         backgroundImage: 'linear-gradient(135deg,#1e1e20,#0e0e11 32%)'
-     }}>
-        <text>
-            hello
-        </text>
-        <div className='h-full translate-y-44'
-             style={{
-                backgroundImage: 'linear-gradient( 55deg,  rgba(150,60,60,1) -11%, rgba(8,0,0,1) 95% )'
-             }}
+      }}
+    >
+        <div className="absolute transform -translate-x-1/2 w-0.5 bg-gray-600 z-50 h-full" style={{ left: '12%'}}></div>
+        <div className="absolute w-0.5 bg-gray-700 z-50" style={{ height: '92%', right: '30%'}}></div>
+        <div className="absolute w-0.5 bg-gray-700 z-50 h-full" style={{ right: '14%'}}></div>
+        <div className="h-full translate-y-48"
+            style={{
+                backgroundImage: 'linear-gradient(90deg, rgba(212,83,83,1) -80%, rgba(0,12,15,1) 100%)'
+            }}
         >
-            <text className="text-blue-600">
-                md:inset-x-[calc(theme(inset.8)+1px)]
-            </text>
+        </div>
+        <div className="absolute inset-16 items-center z-50 pointer-events-none" 
+             style={{
+                backgroundImage: 'linear-gradient(90deg, rgba(234,88,88,1) -30%, rgba(18,53,62,1) 110%)',
+                margin: '2%'
+            }}
+        >
+            <p className="sm:text-4xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-2 px-20">
+                CHARAN'S
+            </p>
         </div>
     </div>
   );
