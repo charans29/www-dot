9c38ab3ed3430123b15d0cd47dfac7370c94fdf2
charans29 <smodugula@csuchico.edu>
2024-07-04 19:45:48 -0700

    dev: logo blinks

--- Components/Content.tsx
+++ Components/Content.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Content() {
   return (
-    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+    <div className="h-screen w-full relative flex items-center justify-center"
+    style={{
+      backgroundImage: 'linear-gradient(360deg, rgba(212,83,83,1) -80%, rgba(0,3,5,1) 15%)'
+    }}
+    >
+        {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div> */}
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent 
+                      bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 dark:bg-grid-small-white/[0.35]">
             Backgrounds
         </p>
     </div>
--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+"use client"
+import React, { useEffect, useState } from 'react';
 
 function Hero() {
+  const [isBlinking, setIsBlinking] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsBlinking(false);
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div
       className="h-screen overflow-hidden"
@@ -8,7 +19,7 @@ function Hero() {
         backgroundImage: 'linear-gradient(135deg,#1e1e20,#0e0e11 32%)'
       }}
     >
-        <div className="absolute transform -translate-x-1/2 w-0.5 bg-gray-600 z-50 h-full" style={{ left: '12%'}}></div>
+        <div className="absolute w-0.5 bg-gray-600 z-50 h-full" style={{ left: '12%'}}></div>
         <div className="absolute w-0.5 bg-gray-700 z-50" style={{ height: '92%', right: '30%'}}></div>
         <div className="absolute w-0.5 bg-gray-700 z-50 h-full" style={{ right: '14%'}}></div>
         <div className="h-full translate-y-48"
@@ -17,19 +28,24 @@ function Hero() {
             }}
         >
         </div>
-        <div className="absolute inset-16 items-center z-50" 
+        <div className="absolute inset-16 z-50" 
              style={{
-                backgroundImage: `linear-gradient(90deg, rgba(234,88,88,0.65) -30%, rgba(18,53,62,1) 110%),
-                                  url('https://as2.ftcdn.net/v2/jpg/01/78/48/61/1000_F_178486172_MyE3t4dYzDHBX1Cwkby4mSbESnbpuqb7.jpg')`,
+                backgroundImage: `linear-gradient(90deg, rgba(234,88,88,0.65) 35%, rgba(18,53,62,1) 110%),
+                                  url('style3.png')`,
                 backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                margin: '2%',
+                backgroundPosition: 'left top 10%',
+                margin: '2%'
             }}
         >
-            <p className="sm:text-4xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-300 py-2 px-20">
-                CHARAN'S
+            <p className={`sm:text-4xl inset-0 z-10 font-bold relative bg-clip-text text-transparent py-2 px-20 ${isBlinking ? 'blink' : 'stable'}`}
+               style={{
+                backgroundImage:'linear-gradient(90deg, rgba(255,255,255,1) 100%, rgba(2,163,200,1) 40%)'
+               }}
+            >
+              [ C H A R A N ' S ]
             </p>
         </div>
+        {/* bg-gradient-to-b from-neutral-200 to-neutral-300 */}
     </div>
   );
 }
